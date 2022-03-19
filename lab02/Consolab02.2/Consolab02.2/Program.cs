847b8ba86470b53d3b1cc9a35f25d89deabd4519
Jowita Jońca <Jowita.Jonca@microsoft.wsei.edu.pl>
2022-03-19 16:44:38 +0100

    lab02

---
+++ lab02/Consolab02.2/Consolab02.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Consolab02._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
