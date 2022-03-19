101ed1845642dc14f64f8538452aebe9743acd86
Jowita Jońca <Jowita.Jonca@microsoft.wsei.edu.pl>
2022-03-19 17:02:50 +0100

    Stworzono klasę person

---
+++ lab02/Consolab02.2/Consolab02.2/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Consolab02._2
+{
+    class Person
+    {
+        private string name;
+        private int age;
+    }
+
+    Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    bool Equals(Person other)
+    {
+        if (other == null) return false;
+        if (other == this) return true;
+
+        return Object.Equals(this.name, other.name) && Object.Equals(this.age, other.age);
+    }
+}
