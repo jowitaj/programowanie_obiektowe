9d575c3715f69073e3098e3fb57c30fadc3443a2
Jowita Jońca <Jowita.Jonca@microsoft.wsei.edu.pl>
2022-03-19 17:03:55 +0100

    Wprowadzono klasę person

--- lab02/Consolab02.2/Consolab02.2/Program.cs
+++ lab02/Consolab02.2/Consolab02.2/Program.cs
@@ -1,39 +1,26 @@
-﻿namespace ConsoleApp
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Consolab02._2
 {
-    public class Program
+    class Person
     {
-        public static void Main()
-        {
-            /*
-            Teacher treacher = new Teacher("Maria Skłodowska", 50);
+        private string name;
+        private int age;
+    }
 
-            Student student1 = new Student("Jan Kowaslski", 21, "LAB-01");
-            Student student2 = new Student("Jan Kowaslski", 21, "LAB-01");
-            Student student3 = new Student("Jaś Fasola", 23, "LAB-02");
+    Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
 
-            student1.AddTask("Taks A", TaskStatus.Waiting);
-            student1.AddTask("Taks B", TaskStatus.Waiting);
-            student1.AddTask("Taks C", TaskStatus.Rejected);
+    bool Equals(Person other)
+    {
+        if (other == null) return false;
+        if (other == this) return true;
 
-            student2.AddTask("Taks A", TaskStatus.Waiting);
-            student2.AddTask("Taks B", TaskStatus.Waiting);
-            student2.AddTask("Taks C", TaskStatus.Rejected);
-
-            student3.AddTask("Taks D", TaskStatus.Done);
-            student3.AddTask("Taks E", TaskStatus.Waiting);
-            student3.AddTask("Taks F", TaskStatus.Waiting);
-
-            student3.UpdateTask(1, TaskStatus.Done);
-            student3.UpdateTask(2, TaskStatus.Progress);
-
-            Person[] persons = { treacher, student1, student2, student3 };
-            Classroom classroom = new Classroom("Sala Komputerowa", persons);
-
-            Console.WriteLine(classroom);
-
-            Console.WriteLine("student1 == student2: " + student1.Equals(student2)); // Output: student1 == student2: true
-            Console.WriteLine("student1 == student3: " + student1.Equals(student3)); // Output: student1 == student3: false
-            */
-        }
+        return Object.Equals(this.name, other.name) && Object.Equals(this.age, other.age);
     }
 }
