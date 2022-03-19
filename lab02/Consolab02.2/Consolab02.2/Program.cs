a62ff710f59bebf560de87fd5792dc9cebf923f7
Jowita Jońca <Jowita.Jonca@microsoft.wsei.edu.pl>
2022-03-19 17:31:18 +0100

    dodano klasę Stusent, Teacher i Task

--- lab02/Consolab02.2/Consolab02.2/Program.cs
+++ lab02/Consolab02.2/Consolab02.2/Program.cs
@@ -4,23 +4,81 @@ using System.Text;
 
 namespace Consolab02._2
 {
-    class Person
+   public class Person:
     {
         private string name;
         private int age;
+
+
+     public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+       public  bool Equals(Person other)
+        {
+            if (other == null) return false;
+            if (other == this) return true;
+
+            return Object.Equals(this.name, other.name) && Object.Equals(this.age, other.age);
+        }
     }
 
-    Person(string name, int age)
+    public class Student
     {
-        this.name = name;
-        this.age = age;
+        private string group;
+        private List tasks;
+        public string group;
+
+        public Student (string name, int age, string group)
+        {
+            this.name = name;
+            this.age = age;
+            this.group = group;
+        }
+
+        public Student(string name, int age, string group, List<Task> tasks)
+        {
+            this.name = name;
+            this.age = age;
+            this.group = group;
+            this.tasks = tasks;
+        }
+
+        //dokończyć
     }
 
-    bool Equals(Person other)
+    public class Teacher
     {
-        if (other == null) return false;
-        if (other == this) return true;
+        public Teacher(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        //dokończyć
+    }
 
-        return Object.Equals(this.name, other.name) && Object.Equals(this.age, other.age);
+    public class Task
+    {
+        private string name;
+        private TaskStatus status;
+        public string name;
+        public TaskStatus status;
+
+        public Task(string name, TaskStatus status)
+        {
+            this.name = name;
+            this.status = status;
+        }
+
+        public bool Equals(Task other)
+        {
+            if (other == null) return false;
+            if (other == this) return true;
+
+            return Object.Equals(this.name, other.name) && Object.Equals(this.status, other.status);
+        }
+        //dokończyć
     }
 }
